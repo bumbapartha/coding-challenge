f6d4d1a262394d7bad2b7279363075f53695ec5d
hibumbapartha <partha.ghosh@vonage.com>
2022-07-23 21:38:24 +0530

    912. Sort an Array - Merge Sort O(nlogn)

--- src/leetcode/M912SortAnArray.java
+++ src/leetcode/M912SortAnArray.java
@@ -17,7 +17,41 @@ public class M912SortAnArray {
 
     class Solution {
         public int[] sortArray(int[] nums) {
-            return nums;
+            return mergeSort(nums, 0, nums.length-1);
+        }
+
+        public int[] mergeSort(int[] nums, int left, int right){
+            if ( left < right ){
+                int mid = (left + right) / 2;
+                int[] array1 = mergeSort(nums, left, mid);
+                int[] array2 = mergeSort(nums, mid+1, right);
+                return merge(array1, array2);
+            }
+            else {
+                return new int[]{nums[left]};
+            }
+        }
+
+        private int[] merge(int[] array1, int[] array2) {
+            int i = 0;
+            int j = 0;
+            int k = 0;
+            int[] array3 = new int[array1.length+array2.length];
+            while(i<array1.length && j<array2.length){
+                if(array1[i]<array2[j]){
+                    array3[k++] = array1[i++];
+                }
+                else {
+                    array3[k++] = array2[j++];
+                }
+            }
+            while(i<array1.length){
+                array3[k++] = array1[i++];
+            }
+            while(j<array2.length) {
+                array3[k++] = array2[j++];
+            }
+            return array3;
         }
     }
 }
