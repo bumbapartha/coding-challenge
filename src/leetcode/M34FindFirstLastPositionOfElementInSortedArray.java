6a82acf4a996f4e294d5b5bc6f14be73f308877c
hibumbapartha <partha.ghosh@vonage.com>
2022-07-25 10:53:26 +0530

    34. Find First and Last Position of Element in Sorted Array binary search Time: O(logn) Space O(logn)

---
+++ src/leetcode/M34FindFirstLastPositionOfElementInSortedArray.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class M34FindFirstLastPositionOfElementInSortedArray {
+    public static void main(String[] args) {
+        M34FindFirstLastPositionOfElementInSortedArray obj = new M34FindFirstLastPositionOfElementInSortedArray();
+        obj.run();
+    }
+
+    private void run() {
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.searchRange(new int[] {}, 0)));
+    }
+
+    class Solution {
+        public int[] searchRange(int[] nums, int target) {
+            int[] index = new int[]{-1, -1};
+
+
+            int found = binarySearchLeft(nums, 0, nums.length, target, 0);
+            if(found >= 0 && found < nums.length && nums[found] == target){
+                int left = binarySearchLeft(nums, 0, nums.length, target, -0.1);
+                int right = binarySearchLeft(nums, 0, nums.length, target, 0.1);
+                index[0] = left;
+                index[1] = right-1;
+            }
+
+
+            return index;
+        }
+        private int binarySearchLeft(int[] nums, int left, int right, int target, double frac){
+            if(left == right){
+                return left;
+            }
+            else if (left < right){
+                int mid = (left + right) /2;
+                if(nums[mid] < ((double) target + frac)) {
+                    return binarySearchLeft(nums, mid+1, right, target, frac);
+                }
+                else {
+                    return binarySearchLeft(nums, left, mid, target, frac);
+                }
+            }
+            else {
+                return -1;
+            }
+        }
+    }
+}
